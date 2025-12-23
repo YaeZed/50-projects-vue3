485e192bb3261bf8bc8820b849d33c4e86076547
YaeZed <1511623546@qq.com>
2025-12-23 21:23:02 +0800

    Skeleton

--- 50projects-vue3/src/App.vue
+++ 50projects-vue3/src/App.vue
@@ -21,7 +21,8 @@
   <!-- <RippleButton></RippleButton> -->
   <!-- <DragAndDrop></DragAndDrop> -->
   <!-- <Kinetic></Kinetic> -->
-  <Ring></Ring>
+  <!-- <Ring></Ring> -->
+   <Skeleton></Skeleton>
 </template>
 
 <script setup lang="ts">
@@ -49,6 +50,7 @@ import DragAndDrop from "./components/DragAndDrop.vue";
 import Kinetic from "./components/Kinetic.vue";
 import test from "./components/Ring.vue";
 import Ring from "./components/Ring.vue";
+import Skeleton from "./components/Skeleton.vue";
 </script>
 
 <style scoped></style>
